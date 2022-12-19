87b5b289481b2ba06aa1298614f52c15bec4bfc0
hjx66 <hjx66@g.cnu.ac.kr>
2022-12-19 10:47:14 +0900

    feat: 전체 메시지 조회 기능 구현 - GET /chat/messages

--- src/main/java/com/chat/chattingapp/controller/ChatContoller.java
+++ src/main/java/com/chat/chattingapp/controller/ChatContoller.java
@@ -3,6 +3,7 @@ package com.chat.chattingapp.controller;
 import com.chat.chattingapp.domain.ChatMessage;
 import com.chat.chattingapp.domain.MessageWriteResponse;
 import com.chat.chattingapp.domain.Result;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,14 @@ import java.util.List;
 public class ChatContoller {
     private List<ChatMessage> chatMessages = new ArrayList<>();
 
+    @GetMapping("/messages")
+    public Result<List<ChatMessage>> getmessages(){
+        return new Result<>(
+                "S-1",
+                "전체 메세지 조회에 성공했습니다.",
+                chatMessages
+        );
+    }
     @PostMapping("/message")
     public Result<MessageWriteResponse> writeMessage(){
         ChatMessage chatMessage = new ChatMessage("홍길동", "안녕하세요.");
